dff47b738b1da4d88d6ae61342601bf15765b2d0
Your Name <junior.alexandre.aj95@mailcom>
2023-05-27 12:35:20 -0300

    editing js file

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,25 @@ let inputcusto = document.getElementById("valor-custo");
 let inputmargem = document.getElementById("valor-margem");
 let inputseguro = document.getElementById("valor-seguro");
 
-inputcusto.addEventListener("change", function (event) {
+function calculator_event (event) {
   let valuecusto = parseFloat(inputcusto.value);
   let valuemargem = parseFloat(inputmargem.value);
   let valueseguro = parseFloat(inputseguro.value);
 
-  console.log(valuecusto);
-  console.log(valuemargem);
+  if(isNaN(valuecusto)){
+    valuecusto = 0;
+  }
 
   let customargem = valuecusto + (valuecusto * valuemargem) / 100;
 
   document.getElementById("totalmargem").innerHTML =
     "R$" + customargem.toFixed(2);
 
-  let custosegurototal = customargem + (customargem + valueseguro / 100);
+  let custosegurototal = customargem + (customargem * valueseguro / 100);
 
   document.getElementById("total").innerHTML =
     "R$" + custosegurototal.toFixed(2);
-});
+}
+inputcusto.addEventListener("input", calculator_event);
+inputmargem.addEventListener("input", calculator_event);
+inputseguro.addEventListener("input", calculator_event);
