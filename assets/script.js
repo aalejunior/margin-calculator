9872a9cdfdff5c8506dfb408f25ad654b7d3a295
Your Name <junior.alexandre.aj95@mailcom>
2023-06-04 20:54:05 -0300

    value fix

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,29 @@ let inputcusto = document.getElementById("valor-custo");
 let inputmargem = document.getElementById("valor-margem");
 let inputseguro = document.getElementById("valor-seguro");
 
-function calculator_event (event) {
+function calculator_event(event) {
   let valuecusto = parseFloat(inputcusto.value);
   let valuemargem = parseFloat(inputmargem.value);
   let valueseguro = parseFloat(inputseguro.value);
 
-  if(isNaN(valuecusto)){
+  if (isNaN(valuecusto)) {
     valuecusto = 0;
   }
 
   let customargem = valuecusto + (valuecusto * valuemargem) / 100;
 
-  document.getElementById("totalmargem").innerHTML =
-    "R$" + customargem.toFixed(2);
+  document.getElementById("totalmargem").innerHTML = customargem.toLocaleString(
+    "pt-BR",
+    { style: "currency", currency: "BRL" }
+  );
 
-  let custosegurototal = customargem + (customargem * valueseguro / 100);
+  let custosegurototal = customargem + (customargem * valueseguro) / 100;
 
-  document.getElementById("total").innerHTML =
-    "R$" + custosegurototal.toFixed(2);
+  document.getElementById("total").innerHTML = custosegurototal.toLocaleString(
+    "pt-BR",
+    { style: "currency", currency: "BRL" }
+  );
 }
 inputcusto.addEventListener("input", calculator_event);
 inputmargem.addEventListener("input", calculator_event);
-inputseguro.addEventListener("input", calculator_event);
+//inputseguro.addEventListener("input", calculator_event);
